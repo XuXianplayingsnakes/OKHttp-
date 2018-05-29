00283864712180d56ba688e9c83850cd887e902c
XuXianplayingsnakes <1565498095@qq.com>
2018-05-29 13:50:26 +0800

    简单测试

--- app/src/main/java/www/wanshe/com/wstore/MainActivity.java
+++ app/src/main/java/www/wanshe/com/wstore/MainActivity.java
@@ -2,6 +2,12 @@ package www.wanshe.com.wstore;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import www.wanshe.com.wstore.bean.Novel;
+import www.wanshe.com.wstore.net.DisposeDataListener;
+import www.wanshe.com.wstore.request.RequestCenter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView textView = (TextView)findViewById(R.id.tv);
+        RequestCenter.requestRecommandData(new DisposeDataListener() {
+            @Override
+            public void onSuccess(Object responseObj) {
+                Novel novel = (Novel) responseObj;
+                textView.setText(novel.getData().get(0).getFemalename());
+            }
 
+            @Override
+            public void onFailure(Object responseObj) {
+                Log.e("测试网络框架", "onFailure: " + responseObj.toString() );
+            }
+        });
     }
 }
